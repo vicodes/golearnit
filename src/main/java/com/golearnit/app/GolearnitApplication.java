09b638895d997f33322407bf99342d00289b8294
Mohit Singh <40341375+vicodes@users.noreply.github.com>
2020-07-04 21:14:46 +0530

    Add files via upload
    initial commit

---
+++ src/main/java/com/golearnit/app/GolearnitApplication.java
@@ -0,0 +1,13 @@
+package com.golearnit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolearnitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolearnitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/golearnit/app/GolearnitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golearnit.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolearnitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
